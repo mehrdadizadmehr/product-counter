691a15edfcc1f06958c7d6c2ed5b426e64ccf5b6
mehrdad <izadmehr.mehrdad@gmail.com>
2024-10-09 14:07:38 +0330

    solve cors issues

--- src/components/ProductCount.js
+++ src/components/ProductCount.js
@@ -1,41 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import { request, gql } from 'graphql-request';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://special-engine.partoir.ir/part/gql';
+
+const GET_TOTAL_PRODUCTS = `
+  query CategoriesList($page: Float!) {
+    partListWithPagination(page: $page) {
+      totalItems
+    }
+  }
+`;
 
 const ProductCount = () => {
-  const [totalItems, setTotalItems] = useState(null);
+  const [totalProducts, setTotalProducts] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const fetchData = async () => {
-    const endpoint = 'http://special-engine.partoir.ir/part/gql';
-    const query = gql`
-      query {
-        partListWithPagination(page: 1) {
-          totalItems
-        }
-      }
-    `;
-
-    try {
-      const data = await request(endpoint, query);
-      setTotalItems(data.partListWithPagination.totalItems);
-    } catch (err) {
-      setError(err);
-      console.error('Error fetching data:', err);
-    }
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const variables = { page: 1 }; // For the first page
+        const response = await axios.post(
+          API_URL,
+          {
+            query: GET_TOTAL_PRODUCTS,
+            variables: variables,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*', // هدر برای حل مشکل CORS
+            },
+          }
+        );
+
+        setTotalProducts(response.data.data.partListWithPagination.totalItems);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+
     fetchData();
   }, []);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching data: {error.message}</p>;
+
   return (
     <div>
-      {error && <p>Error fetching data: {error.message}</p>}
-      {totalItems !== null ? (
-        <p>Total Products: {totalItems}</p>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <h1>Total Products: {totalProducts}</h1>
     </div>
   );
 };
